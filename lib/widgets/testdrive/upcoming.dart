64d2271cfa43d761e5994b0fb2b43cf471ae7ce3
Saad <saad.ansari@ariantechsolutions.com>
2025-09-30 12:28:59 +0530

    working fine

--- lib/widgets/testdrive/upcoming.dart
+++ lib/widgets/testdrive/upcoming.dart
@@ -109,16 +109,13 @@ class _TestUpcomingState extends State<TestUpcoming> {
     String leadId = item['lead_id'] ?? '';
     Navigator.push(
       context,
-      //   MaterialPageRoute(
-      //     builder: (context) => TestdriveVerifyotp(
-      //       email: email,
-      //       mobile: mobile,
-      //       leadId: leadId,
-      //       eventId: eventId,
-      //     ),
-      //   ),
       MaterialPageRoute(
-        builder: (context) => Feedbackscreen(leadId: leadId, eventId: eventId),
+        builder: (context) => TestdriveVerifyotp(
+          email: email,
+          mobile: mobile,
+          leadId: leadId,
+          eventId: eventId,
+        ),
       ),
     );
     print("Call action triggered for ${item['name']}");
